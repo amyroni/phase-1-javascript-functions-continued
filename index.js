485b1b1b8f3fbbada7e6f5be71b5ed97186f7c37
Amy Shi <amyhshi92@gmail.com>
2022-02-22 13:46:53 -0500

    commit tree 3627a994715aa5730694458e0113f8f14508452e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(s = "roller-skate") {
+    return `This Saturday, I want to ${s}!`;
+}
+
+const mondayWork = function (s = "go to the office") {
+    return `This Monday, I will ${s}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
